f3a5d60d246e963053e1fb53c24ae13a281c4494
rominabr <romina.brutti@gmail.com>
2022-10-27 23:03:27 -0300

    Motor

--- Juegos/Naves/Player/Player.gd
+++ Juegos/Naves/Player/Player.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 ## Atributos Export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
+export var estela_maxima:int = 150
 
 ## Atributos
 var empuje:Vector2 = Vector2.ZERO
@@ -12,6 +13,8 @@ var dir_rotacion:int = 0
 ## Atributos onready
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
+onready var estela:Estela = $EstelaPuntoInicio/Trail2D
+onready var motor_sfx:Motor = $MotorSFX
 
 ## Metodos
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
@@ -21,10 +24,23 @@ func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	#Disparo Rayo
 	if event.is_action_pressed("disparo_secundario"):
-		laser.set_is_casting(true)
+		laser.set_is_casting(true)		
+		
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
 	
+	# Control estela
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+		
+	elif event.is_action_pressed("mover_atras"):
+		motor_sfx.sonido_on()
+		estela.set_max_points(0)		
+	
+	if event.is_action_released("mover_adelante") or event.is_action_released("mover_atras"):
+		motor_sfx.sonido_off()	
+		
 func _process(_delta: float) -> void:
 	player_input()
 	
