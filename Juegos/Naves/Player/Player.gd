f8e5dafb10ad89b41fa7677f161ae82a58da4af1
rominabr <romina.brutti@gmail.com>
2022-10-27 23:43:23 -0300

    Maquina de estados finitos

--- Juegos/Naves/Player/Player.gd
+++ Juegos/Naves/Player/Player.gd
@@ -1,6 +1,9 @@
 class_name Player
 extends RigidBody2D
 
+## Enums
+enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
+
 ## Atributos Export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
@@ -9,19 +12,28 @@ export var estela_maxima:int = 150
 ## Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
+var estado_actual:int = ESTADO.SPAWN
 
 ## Atributos onready
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
+onready var colisionador:CollisionShape2D = $CollisionShape2D
 
 ## Metodos
+func _ready() -> void:
+	controlador_estados(estado_actual)
+	controlador_estados(ESTADO.VIVO)
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not esta_input_activo():
+		return
+		
 	#Disparo Rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)		
@@ -45,7 +57,35 @@ func _process(_delta: float) -> void:
 	player_input()
 	
 ## Metodos Custom
+func controlador_estados(nuevo_estado:int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+			
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(true)
+			queue_free()
+		_:
+			print("Error de estado")
+	
+	estado_actual = nuevo_estado
+
+func esta_input_activo() -> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	return true
+
 func player_input() -> void:
+	if not esta_input_activo():
+		return
+			
 	## Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
