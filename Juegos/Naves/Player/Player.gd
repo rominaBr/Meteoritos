d98bc5051bac97660d04f1f3cf27b8ddb3982835
rominabr <romina.brutti@gmail.com>
2022-10-31 21:54:11 -0300

    Muerte

--- Juegos/Naves/Player/Player.gd
+++ Juegos/Naves/Player/Player.gd
@@ -71,6 +71,7 @@ func controlador_estados(nuevo_estado:int) -> void:
 		ESTADO.MUERTO:
 			colisionador.set_deferred("disabled", true)
 			canion.set_puede_disparar(true)
+			Eventos.emit_signal("nave_destruida",global_position, 3)
 			queue_free()
 		_:
 			print("Error de estado")
@@ -111,3 +112,6 @@ func player_input() -> void:
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "spawn":
 		controlador_estados(ESTADO.VIVO)
+
+func destruir() -> void:
+	controlador_estados(ESTADO.MUERTO)
--- Juegos/Niveles/Nivel.gd
+++ Juegos/Niveles/Nivel.gd
@@ -1,6 +1,8 @@
 class_name Nivel
 extends Node2D
 
+## Atributos export
+export var explosion:PackedScene = null
 
 ## Atributos onready
 onready var contenedor_proyectiles:Node
@@ -13,6 +15,7 @@ func _ready() -> void:
 ## Medodos custom
 func conectar_seniales() -> void:
 	Eventos.connect("disparo", self, "_on_disparo")
+	Eventos.connect("nave_destruida", self, "_on_nave_destruida")
 
 func crear_contenedores() -> void:
 	contenedor_proyectiles = Node.new()
@@ -22,5 +25,10 @@ func crear_contenedores() -> void:
 func _on_disparo(proyectil:Proyectil) -> void:
 	add_child(proyectil)
 	
-
+func _on_nave_destruida(posicion: Vector2, num_explosiones) -> void:
+	for i in range(num_explosiones):
+		var new_explosion:Node2D = explosion.instance()
+		new_explosion.global_position = posicion
+		add_child(new_explosion)
+		yield(get_tree().create_timer(0.6),"timeout")
 
