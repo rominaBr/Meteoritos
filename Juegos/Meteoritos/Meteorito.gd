e596956b4c1fac1ef13f34055f01fe2cc1dc60f6
rominabr <romina.brutti@gmail.com>
2022-11-07 21:52:55 -0300

    Intervalo de spawn

--- Juegos/Meteoritos/Meteorito.gd
+++ Juegos/Meteoritos/Meteorito.gd
@@ -11,10 +11,18 @@ onready var impacto_sfx:AudioStreamPlayer2D = $impactoSFX
 
 ## Atributos
 var hitpoints:float
+var esta_en_sector:bool = true setget set_esta_en_sector
+var pos_spawn_original:Vector2
+var vel_spawn_original:Vector2
+
+## Setters y Getters
+func set_esta_en_sector(valor:bool) -> void:
+	esta_en_sector = valor
 
 ## Constructor
 func crear(pos: Vector2, dir: Vector2, tamanio: float) -> void:
 	position = pos
+	pos_spawn_original = position
 	#Calcular masa, tamaño de sprite y de colisionador
 	mass *= tamanio
 	$Sprite.scale = Vector2.ONE * tamanio
@@ -25,6 +33,7 @@ func crear(pos: Vector2, dir: Vector2, tamanio: float) -> void:
 	$CollisionShape2D.shape = forma_colision
 	#Calcular velocidades	
 	linear_velocity = (vel_lineal_base * dir / tamanio) * aleatorizar_velocidad()
+	vel_spawn_original = linear_velocity
 	angular_velocity = (vel_ang_base / tamanio) * aleatorizar_velocidad()
 	#Calcular hitpoints
 	hitpoints = hitpoints_base * tamanio	
@@ -33,6 +42,16 @@ func crear(pos: Vector2, dir: Vector2, tamanio: float) -> void:
 ## Métodos
 func _ready() -> void:	
 	angular_damp = vel_ang_base
+	
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	if esta_en_sector:
+		return
+	
+	var mi_transform := state.get_transform()
+	mi_transform.origin = pos_spawn_original
+	linear_velocity = vel_spawn_original
+	state.set_transform(mi_transform)
+	esta_en_sector = true
 	
 ##Métodos Custom
 func recibir_danio(danio:float) -> void:	
--- Juegos/Meteoritos/SectorMeteoritos.gd
+++ Juegos/Meteoritos/SectorMeteoritos.gd
@@ -3,24 +3,31 @@ extends Node2D
 
 ## Atributos export
 export var cantidad_meteoritos:int = 10
+export var intervalo_spawn:float = 1.2
 
 ## Atributos
 var spawners:Array
 
 ## Métodos
 func _ready() -> void:
-	$AnimationPlayer.play("advertencia")	
+	$AnimationPlayer.play("default")
 	almacenar_spawners()
+	conectar_seniales_detectores()
+	$SpawnTimer.wait_time = intervalo_spawn
 
 ## Métodos Custom	
-func almacenar_spawners() -> void:	
+func almacenar_spawners() -> void:
+	$AnimationPlayer.play("advertencia")	
 	for spawner in $Spawners.get_children():		
 		spawners.append(spawner)
 		
 func spawner_aleatorio() -> int:
 	randomize()
 	return randi() % spawners.size()
-	
+
+func conectar_seniales_detectores() -> void:
+	for detector in $DetectorFueraZona.get_children():
+		detector.connect("body_entered", self, "_on_detector_body_entered")	
 
 ## Señales internas
 func _on_SpawnTimer_timeout() -> void:	
@@ -30,3 +37,6 @@ func _on_SpawnTimer_timeout() -> void:
 		return		
 	spawners[spawner_aleatorio()].spawnear_meteorito()
 	cantidad_meteoritos -= 1
+
+func _on_detector_body_entered(body: Node) -> void:	
+	body.set_esta_en_sector(false)
