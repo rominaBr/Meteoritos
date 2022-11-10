f2aad5771adc605ebfa41d3b414ff725ded0174c
rominabr <romina.brutti@gmail.com>
2022-11-10 01:47:06 -0300

    Herencia

--- Juegos/Naves/Player/CamaraPlayer.gd
+++ Juegos/Naves/Player/CamaraPlayer.gd
@@ -1,5 +1,5 @@
 class_name CamaraPlayer
-extends Camera2D
+extends CamaraJuego
 
 ## Variables export
 export var variacion_zoom:float = 0.1
@@ -7,21 +7,11 @@ export var zoom_minimo:float = 0.8
 export var zoom_maximo:float = 1.5
 
 
-## Variables onready
-onready var tween_zoom:Tween = $TweenZoom
-
-## Atributos
-var zoom_original:Vector2
-var puede_hacer_zoom:bool= true setget set_puede_hacer_zoom
-
 ## Setters y getters
 func set_puede_hacer_zoom(puede:bool) -> void:
 	puede_hacer_zoom = puede
 
 ## Métodos
-func _ready() -> void:
-	zoom_original = zoom
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("zoom_acercar"):
 		controlar_zoom(-variacion_zoom)
@@ -33,19 +23,3 @@ func controlar_zoom(mod_zoom: float) -> void:
 	var zoom_x = clamp(zoom.x + mod_zoom, zoom_minimo, zoom_maximo)
 	var zoom_y = clamp(zoom.y + mod_zoom, zoom_minimo, zoom_maximo)
 	zoom_suavizado(zoom_x, zoom_y, 0.15)
-
-func zoom_suavizado(nuevo_zoom_x: float, nuevo_zoom_y: float, tiempo_transicion:float) -> void:
-	tween_zoom.interpolate_property(
-		self,
-		"zoom",
-		zoom,
-		Vector2(nuevo_zoom_x, nuevo_zoom_y),
-		tiempo_transicion,
-		Tween.TRANS_LINEAR,
-		Tween.EASE_IN_OUT
-	)
-	tween_zoom.start()
-
-func devolver_zoom_original() -> void:
-	puede_hacer_zoom = false
-	zoom_suavizado(zoom_original.x, zoom_original.y, 1.0)
---
+++ Juegos/Niveles/CamaraJuego.gd
@@ -0,0 +1,34 @@
+class_name CamaraJuego
+extends Camera2D
+
+## Atributos
+var zoom_original:Vector2
+var puede_hacer_zoom:bool= true setget set_puede_hacer_zoom
+
+## Variables onready
+onready var tween_zoom:Tween = $TweenZoom
+
+## Setters y getters
+func set_puede_hacer_zoom(puede:bool) -> void:
+	puede_hacer_zoom = puede
+
+## Métodos
+func _ready() -> void:
+	zoom_original = zoom
+
+## Métodos custom
+func devolver_zoom_original() -> void:
+	puede_hacer_zoom = false
+	zoom_suavizado(zoom_original.x, zoom_original.y, 1.0)
+
+func zoom_suavizado(nuevo_zoom_x: float, nuevo_zoom_y: float, tiempo_transicion:float) -> void:
+	tween_zoom.interpolate_property(
+		self,
+		"zoom",
+		zoom,
+		Vector2(nuevo_zoom_x, nuevo_zoom_y),
+		tiempo_transicion,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_IN_OUT
+	)
+	tween_zoom.start()
