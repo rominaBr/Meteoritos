c5224cdaebd6ec70b57043a1c5e2a92569c617ea
rominabr <romina.brutti@gmail.com>
2022-11-12 00:18:35 -0300

    Efectos de sonido

--- Juegos/Estaciones/EstacionRecarga.gd
+++ Juegos/Estaciones/EstacionRecarga.gd
@@ -5,6 +5,9 @@ extends Node2D
 export var energia:float = 6.0
 export var radio_energia_entregada:float = 0.05
 
+##Atributos onready
+onready var carga_sfx:AudioStreamPlayer = $CargaSFX
+
 ## Atributos
 var nave_player:Player = null
 var player_en_zona:bool = false
@@ -14,20 +17,27 @@ func _unhandled_input(event: InputEvent) -> void:
 	if not puede_recargar(event):
 		return
 	
-	energia -= radio_energia_entregada
-	print("Energía estación: ", energia)
+	controlar_energia()	
 		
 	if event.is_action("recarga_escudo"):
 		nave_player.get_escudo().controlar_energia(radio_energia_entregada)
 	elif event.is_action("recarga_laser"):
 		nave_player.get_laser().controlar_energia(radio_energia_entregada)
 
+	
 func puede_recargar(event: InputEvent) -> bool:
 	var hay_input = event.is_action("recarga_escudo") or event.is_action("recarga_laser")
 	if hay_input and player_en_zona and energia > 0.0:
+		if !carga_sfx.playing:
+			carga_sfx.play()
 		return true
 	
 	return false
+
+func controlar_energia() -> void:
+	energia -= radio_energia_entregada
+	if energia <= 0.0:
+		$VacioSfx.play()
 
 ## Señales internas
 func _on_AreaColision_body_entered(body: Node) -> void:
