70a29f633ccce862c85a92116c48e72ff1cf6b66
rominabr <romina.brutti@gmail.com>
2022-11-14 01:27:28 -0300

    Spawn en un punto determinado

--- Juegos/Estaciones/BaseEnemiga.gd
+++ Juegos/Estaciones/BaseEnemiga.gd
@@ -15,6 +15,7 @@ var esta_destruida:bool = false
 func _ready() -> void:
 	$AnimationPlayer.play(elegir_animacion_aleatoria())
 
+
 ## Métodos custom
 func elegir_animacion_aleatoria() -> String:
 	randomize()
@@ -42,6 +43,36 @@ func destruir() -> void:
 	Eventos.emit_signal("base_destruida", posicion_partes)
 	queue_free()
 
+func spawnear_orbital() -> void:
+	var pos_spawn:Vector2 = deteccion_cuadrante()
+	var new_orbital:EnemigoOrbital = orbital.instance()
+	new_orbital.crear(
+		global_position + pos_spawn,
+		self
+	)
+	Eventos.emit_signal("spawn_orbital", new_orbital)
+	
+func deteccion_cuadrante() -> Vector2:
+	var player_objetivo:Player = DatosJuego.get_player_actual()
+	
+	if not player_objetivo:
+		return Vector2.ZERO
+		
+	var dir_player:Vector2 = player_objetivo.global_position - global_position
+	var angulo_player:float = rad2deg(dir_player.angle())
+	
+	if abs(angulo_player) <= 45.0:
+		return $PosionesSpawn/Este.position 
+	elif abs(angulo_player) > 135.0 and abs(angulo_player) <= 180.0:
+		return $PosicionesSpawn/Oeste.position
+	elif abs(angulo_player) > 45.0 and abs(angulo_player) <= 135.0:
+		if sign(angulo_player) > 0:
+			return $PosicionesSpawn/Sur.position 
+		else:
+			return $PosicionesSpawn/Norte.position 
+	
+	return $PosicionesSpawn/Norte.position
+	
 func _on_AreaColision_body_entered(body: Node) -> void:
 	if body.has_method("destruir"):
 		body.destruir()
@@ -49,10 +80,5 @@ func _on_AreaColision_body_entered(body: Node) -> void:
 
 func _on_VisibilityNotifier2D_screen_entered() -> void:
 	$VisibilityNotifier2D.queue_free()
+	spawnear_orbital()
 	
-	var new_orbital:EnemigoOrbital = orbital.instance()
-	new_orbital.crear(
-		global_position + $PosicionesSpawn/Norte.global_position,
-		self
-	)
-	Eventos.emit_signal("spawn_orbital", new_orbital)
