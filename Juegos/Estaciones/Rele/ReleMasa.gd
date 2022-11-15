1fc99e649fcb7e81806598e77783a4b6d592f8d5
rominabr <romina.brutti@gmail.com>
2022-11-14 21:35:40 -0300

    Super Activando

--- Juegos/Estaciones/Rele/ReleMasa.gd
+++ Juegos/Estaciones/Rele/ReleMasa.gd
@@ -7,3 +7,8 @@ extends Node2D
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "spawn":
 		$AnimationPlayer.play("activado")
+
+func _on_DetectorPlayer_body_entered(body: Node) -> void:
+	$DetectorPlayer/CollisionShape2D.set_deferred("disabled", true)
+	$AnimationPlayer.play("super_activado")
+	body.desactivar_controles()
